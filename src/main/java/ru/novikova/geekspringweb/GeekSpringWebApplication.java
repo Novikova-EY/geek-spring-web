154fc82c5f9c5d08cafad8431719fe3ffe8cac21
Novikova-EY <75306313+Novikova-EY@users.noreply.github.com>
2021-12-29 14:04:33 +0300

    Add files via upload

---
+++ src/main/java/ru/novikova/geekspringweb/GeekSpringWebApplication.java
@@ -0,0 +1,13 @@
+package ru.novikova.geekspringweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekSpringWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeekSpringWebApplication.class, args);
+	}
+
+}
